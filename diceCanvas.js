20722087086737f13e4df94307bfa975f88cc367
steeve mellet <melletsteeve@orange.fr>
2022-01-18 12:17:22 +0100

    implémentation du JS et son affichage

--- diceCanvas.js
+++ diceCanvas.js
@@ -1,15 +1,12 @@
-function entierAleatoire(min, max)
-{
- return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-
 //dé 1
 
 const diceOne = () => {
-const un = document.getElementById('un');
+const un = document.getElementById('diceCanvas');
 let ctx = un.getContext('2d');
+ctx.beginPath()
+ctx.fillStyle= 'white'
+ctx.fillRect(0, 0, 100, 100)
+ctx.fill()
 
 ctx.beginPath()
 ctx.arc(50, 50, 10, 0, Math.PI * 2);
@@ -20,9 +17,12 @@ ctx.fill()
 
 //dé 2
 const diceTwo = () => {
-const deux = document.getElementById('un');
+const deux = document.getElementById('diceCanvas');
 let ctx2 = deux.getContext('2d');
-
+ctx2.beginPath()
+ctx2.fillStyle= 'white'
+ctx2.fillRect(0, 0, 100, 100)
+ctx2.fill()
 ctx2.beginPath()
 ctx2.arc(70, 30, 9, 0, Math.PI * 2);
 ctx2.fillStyle = 'red';
@@ -34,8 +34,13 @@ ctx2.fill()
 
 //dé 3
 const diceThree = () => {
-const trois = document.getElementById('un');
+const trois = document.getElementById('diceCanvas');
 let ctx3 = trois.getContext('2d');
+
+ctx3.beginPath()
+ctx3.fillStyle= 'white'
+ctx3.fillRect(0, 0, 100, 100)
+ctx3.fill()
 
 ctx3.beginPath()
 ctx3.arc(75, 25, 8, 0, Math.PI * 2);
@@ -52,8 +57,13 @@ ctx3.fill()
 //dé 4
 
 const diceFour = () => {
-const quatre = document.getElementById('un');
+const quatre = document.getElementById('diceCanvas');
 let ctx4 = quatre.getContext('2d');
+
+ctx4.beginPath()
+ctx4.fillStyle= 'white'
+ctx4.fillRect(0, 0, 100, 100)
+ctx4.fill()
 
 ctx4.beginPath()
 ctx4.arc(25, 75, 8, 0, Math.PI * 2);
@@ -75,8 +85,13 @@ ctx4.fill()
 
 //dé 5
 const diceFive = () => {
-const cinq = document.getElementById('un');
+const cinq = document.getElementById('diceCanvas');
 let ctx5 = cinq.getContext('2d');
+
+ctx5.beginPath()
+ctx5.fillStyle= 'white'
+ctx5.fillRect(0, 0, 100, 100)
+ctx5.fill()
 
 ctx5.beginPath()
 ctx5.arc(75, 25,8, 0, Math.PI * 2);
@@ -102,8 +117,13 @@ ctx5.fill()
 //dé 6
 const diceSix = () => {
 
-const six = document.getElementById('un');
+const six = document.getElementById('diceCanvas');
 let ctx6 = six.getContext('2d');
+
+ctx6.beginPath()
+ctx6.fillStyle= 'white'
+ctx6.fillRect(0, 0, 100, 100)
+ctx6.fill()
 
 ctx6.beginPath()
 ctx6.arc(30, 25, 8, 0, Math.PI * 2);
@@ -127,28 +147,45 @@ ctx6.fillStyle = 'red'
 ctx6.fill()
 }
 
-const dice = document.getElementById('bouton')
 
-dice.addEventListener('click', function() {
-  let entier = entierAleatoire(1, 6)
-  console.log(entier);
-  if (entier == 1){
-    diceOne()
-  }
-  else if (entier == 2){
-    diceTwo()
-  }
-  else if (entier == 3){
-    diceThree()
-  }
-  else if (entier == 4){
-    diceFour()
-  }
-  else if (entier == 5){
-    diceFive()
-  }
-  else if (entier == 6){
-    diceSix()
-  }
+const isPlayer1Active = () => {
+  const isPlayer1 = document.getElementById('isPlayer1Active')
+  let context = isPlayer1.getContext('2d')
 
-})
+  context.beginPath()
+  context.arc(30, 30, 8, 0, Math.PI * 2);
+  context.fillStyle = 'red'
+  context.fill()
+}
+
+const isPlayer1NoActive = () => {
+  const isPlayer1 = document.getElementById('isPlayer1Active')
+  let context = isPlayer1.getContext('2d')
+
+  context.beginPath()
+  context.arc(30, 30, 8, 0, Math.PI * 2);
+  context.fillStyle = 'lightgrey'
+  context.fill()
+}
+
+const isPlayer2active = () => {
+  
+  const isPlayer2 = document.getElementById('isPlayer2Active')
+  let context = isPlayer2.getContext('2d')
+
+  context.beginPath()
+  context.arc(30, 30, 8, 0, Math.PI * 2);
+  context.fillStyle = 'red'
+  context.fill()
+}
+
+const isPlayer2NoActive = () => {
+  const isPlayer2 = document.getElementById('isPlayer2Active')
+  let context = isPlayer2.getContext('2d')
+
+  context.beginPath()
+  context.arc(30, 30, 8, 0, Math.PI * 2);
+  context.fillStyle = 'white'
+  context.fill()
+}
+
---
+++ script.js
@@ -0,0 +1,220 @@
+function entierAleatoire(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function game() {
+
+
+
+  let scoreGlobalPlayer1 = 0
+  let scoreGlobalPlayer2 = 0
+  let tour = 1
+
+  function scoreGlobalPlayer21() {
+    document.getElementById('scoreGlobalPlayer2').innerHTML = scoreGlobalPlayer2
+  }
+  
+  function scoreGlobalPlayer11() {
+    document.getElementById('scoreGlobalPlayer1').innerHTML = scoreGlobalPlayer1
+  }
+  playGame()
+
+  
+  function playGame(){
+    let scoreRoundPlayer1 = 0
+    let scoreRoundPlayer2 = 0
+    tour = tour + 1
+
+    //fonctions pour l'affichage des différents scores
+    function scoreRoundPlayer11() {
+      document.getElementById('scoreRoundPlayer1').innerHTML = scoreRoundPlayer1
+    }
+    
+    function scoreRoundPlayer21() {
+      document.getElementById('scoreRoundPlayer2').innerHTML = scoreRoundPlayer2
+    }
+
+  
+    scoreGlobalPlayer11()
+    scoreRoundPlayer11()
+    scoreGlobalPlayer21()
+    scoreRoundPlayer21()
+    console.log(`tour : ${tour}`)
+  
+    const play1 = () => {
+      isPlayer2NoActive()
+      isPlayer1Active()
+      console.log(`global J1 début tour play 1 : ${scoreGlobalPlayer1}`);
+  
+      const dicePlay1 = document.getElementById('rollDice')
+      dicePlay1.addEventListener('click', diceClicked) 
+    
+      const holdPlayer1 = document.getElementById('hold')
+      holdPlayer1.addEventListener('click', holdClicked)
+    
+      function diceClicked(){
+    
+        let entier = entierAleatoire(1, 6)
+        console.log(entier);
+        if (entier == 1){
+          diceOne()
+          scoreRoundPlayer1 = 0
+          dicePlay1.removeEventListener('click', diceClicked)
+          holdPlayer1.removeEventListener('click', holdClicked)
+          playGame()
+        }
+        else if (entier == 2){
+          diceTwo()
+          scoreRoundPlayer1 = scoreRoundPlayer1 + 2
+        }
+        else if (entier == 3){
+          diceThree()
+          scoreRoundPlayer1 = scoreRoundPlayer1 + 3
+        }
+        else if (entier == 4){
+          diceFour()
+          scoreRoundPlayer1 = scoreRoundPlayer1 + 4
+        }
+        else if (entier == 5){
+          diceFive()
+          scoreRoundPlayer1 = scoreRoundPlayer1 + 5
+        }
+        else {
+          diceSix()
+          scoreRoundPlayer1 = scoreRoundPlayer1 + 6
+        }
+        scoreRoundPlayer11()
+      }
+      
+      
+      function holdClicked(){
+        scoreGlobalPlayer1 = scoreGlobalPlayer1 + scoreRoundPlayer1
+        scoreRoundPlayer1 = 0
+        console.log('hold player1');
+        
+        holdPlayer1.removeEventListener('click', holdClicked)
+        dicePlay1.removeEventListener('click', diceClicked)
+        scoreGlobalPlayer11()
+        scoreRoundPlayer11()
+        playGame()
+      }
+
+      
+    }
+  
+    const play2 = () => {
+      isPlayer1NoActive()
+      isPlayer2active()
+      
+      console.log(`global J2 début tour play 1 : ${scoreGlobalPlayer2}`);
+
+      const dicePlay2 = document.getElementById('rollDice')
+      dicePlay2.addEventListener('click', diceClicked2) 
+    
+      const holdPlayer2 = document.getElementById('hold')
+      holdPlayer2.addEventListener('click', holdClicked2)
+    
+      function diceClicked2(){
+    
+        let entier = entierAleatoire(1, 6)
+        console.log(entier);
+        if (entier == 1){
+          diceOne()
+          scoreRoundPlayer2 = 0
+          dicePlay2.removeEventListener('click', diceClicked2)
+          holdPlayer2.removeEventListener('click', holdClicked2)
+          playGame()
+        }
+        else if (entier == 2){
+          diceTwo()
+          scoreRoundPlayer2 = scoreRoundPlayer2 + 2
+        }
+        else if (entier == 3){
+          diceThree()
+          scoreRoundPlayer2 = scoreRoundPlayer2 + 3
+        }
+        else if (entier == 4){
+          diceFour()
+          scoreRoundPlayer2 = scoreRoundPlayer2 + 4
+        }
+        else if (entier == 5){
+          diceFive()
+          scoreRoundPlayer2 = scoreRoundPlayer2 + 5
+        }
+        else {
+          diceSix()
+          scoreRoundPlayer2 = scoreRoundPlayer2 + 6
+        }
+        scoreRoundPlayer21()
+      }
+      
+
+      function holdClicked2(){
+        scoreGlobalPlayer2 = scoreGlobalPlayer2 + scoreRoundPlayer2
+        scoreRoundPlayer2 = 0
+        console.log('hold player 2');
+        holdPlayer2.removeEventListener('click', holdClicked2)
+        dicePlay2.removeEventListener('click', diceClicked2)
+        scoreGlobalPlayer21()
+        scoreRoundPlayer21()
+        playGame()
+        
+      }
+    
+      
+    }
+  
+  
+  
+
+
+
+    
+
+    //Conditions de fin de partie
+    if ((scoreGlobalPlayer1 > 99) || (scoreGlobalPlayer2 > 99)){
+      scoreGlobalPlayer1 = 0
+      scoreGlobalPlayer2 = 0
+      alert('un des 2 joueurs gagne')
+      game()
+    }
+
+    
+    else {
+      if(tour%2 == 0){
+        play1()
+      }
+      else {
+        play2()
+      }
+
+    }
+  }
+  
+}
+
+
+
+game()
+
+
+const newGame = document.getElementById('newGame')
+
+newGame.addEventListener('click', game)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
